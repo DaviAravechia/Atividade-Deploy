166e45aa73b03e01bca45d6c69387d9bbb1bcc68
DaviAravechia <alferdavi@gmail.com>
2024-11-04 09:43:14 -0300

    atualizacao table livros

--- api/models.py
+++ api/models.py
@@ -1,9 +1,9 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
 class Livro (models.Model):
     nome = models.CharField(max_length=100)
-    autor = models.CharField(max_length=100)
-    vendas = models.IntegerField()
+    data_de_publicacao = date.Today()
     def __str__(self):
         return self.nome
