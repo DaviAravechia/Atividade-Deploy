01896ab936c52775cb8bc71462ed30c9230cbeb6
DaviAravechia <alferdavi@gmail.com>
2024-11-04 09:47:54 -0300

    att table livros

--- api/models.py
+++ api/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Livro (models.Model):
     nome = models.CharField(max_length=100)
-    data_de_publicacao = date.Today()
+    data_de_publicacao = date.today()
     def __str__(self):
         return self.nome
